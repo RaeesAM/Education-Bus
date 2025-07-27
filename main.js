968fb5a0fd135b8906a33249de2efcf65c0a9102
Muhammed Raees <raees573184@gmail.com>
2025-07-27 08:50:19 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,66 @@
+
+
+const scrollRevealOption = {
+    origin: "bottom",
+    distance: "50px",
+    duration: 1000,
+  };
+  
+  // Correct way to use ScrollReveal:
+  ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+  });
+
+  ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  
+  ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+
+  ScrollReveal().reveal(".header__btns", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+
+  ScrollReveal().reveal(".destination__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+    ScrollReveal().reveal(".showcase__image img", {
+    ...scrollRevealOption,
+    origin: "left",
+  });
+
+  ScrollReveal().reveal(".showcase__content h4", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  
+  ScrollReveal().reveal(".showcase__content p", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+
+  ScrollReveal().reveal("showcase__btn", {
+    ...scrollRevealOption,
+    delay: 1500,
+     });
+
+       ScrollReveal().reveal(".banner__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  ScrollReveal().reveal(".discover__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+
+
